7c137c232172b6cbecceb18fee72933e00b2133a
emastra <emiliano.mastra@gmail.com>
2023-09-10 14:03:33 +0200

    comments; @use;

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-import App from './app/App';
 import './index.scss';
+import App from './app/App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
