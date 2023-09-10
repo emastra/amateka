d3415c241b6b551e2dff6e7b86af047182c2ad46
emastra <emiliano.mastra@gmail.com>
2023-09-10 23:39:59 +0100

    minor

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import './index.scss';
+
 import App from './app/App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
