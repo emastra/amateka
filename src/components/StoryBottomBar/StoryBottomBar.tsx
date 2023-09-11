e10d8be3d1c95fc1c1201c7bab157c657cd746b0
emastra <emiliano.mastra@gmail.com>
2023-09-11 15:36:00 +0100

    StoryBottomBar styling

--- src/components/StoryBottomBar/StoryBottomBar.tsx
+++ src/components/StoryBottomBar/StoryBottomBar.tsx
@@ -4,17 +4,21 @@ const StoryBottomBar = () => {
     return (
         <>
             <div id='storybottombar'>
-                <div className='time-menu-wrapper'>
-                    <img className='time-menu-icon' src='/src/assets/svg/time-menu.svg' alt='time-menu' />
+                <div className='storybottombar-icon-button'>
+                    <img id='event-icon' src='/src/assets/svg/event-icon.svg' alt='Events' />
+                    <div className='storybottombar-label'>Events</div>
                 </div>
-                <div className='time-slider'>
-                    <img className='more-icon' src='/src/assets/svg/more-icon.svg' alt='more-options' />
+                <div className='storybottombar-icon-button'>
+                    <img id='years-icon' src='/src/assets/svg/rising-sun-icon.svg' alt='Years' />
+                    <div className='storybottombar-label'>Years</div>
                 </div>
-                <div className='more-wrapper'>
-                    <img className='more-icon' src='/src/assets/svg/more-icon.svg' alt='more-options' />
+                <div className='storybottombar-icon-button'>
+                    <img id='decades-icon' src='/src/assets/svg/decade-icon.svg' alt='Decades' />
+                    <div className='storybottombar-label'>Decades</div>
                 </div>
-                <div>
-                    <img className='more-icon' src='/src/assets/svg/more-icon.svg' alt='more-options' />
+                <div className='storybottombar-icon-button'>
+                    <img id='concepts-icon' src='/src/assets/svg/brain-icon.svg' alt='Concepts' />
+                    <div className='storybottombar-label'>Concepts</div>
                 </div>
             </div>
         </>
--- src/components/StoryLayout/StoryLayout.tsx
+++ src/components/StoryLayout/StoryLayout.tsx
@@ -2,13 +2,14 @@ import './StoryLayout.scss';
 import { Outlet } from 'react-router-dom';
 
 import StoryBar from '../StoryBar/StoryBar';
+import StoryBottomBar from '../StoryBottomBar/StoryBottomBar';
 
 const StoryLayout = () => {
     return (
         <>
             <StoryBar />
             <Outlet />
-            <div>test 2</div>
+            <StoryBottomBar />
         </>
     );
 };
