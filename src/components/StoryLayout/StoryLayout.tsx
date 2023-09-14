c606e60c4789653fb3a865547c203f58af00a13c
emastra <emiliano.mastra@gmail.com>
2023-09-14 16:58:49 +0100

    add time menu - wip

--- src/components/StoryLayout/StoryLayout.tsx
+++ src/components/StoryLayout/StoryLayout.tsx
@@ -3,11 +3,13 @@ import { Outlet } from 'react-router-dom';
 
 import StoryBar from '../StoryBar/StoryBar';
 import StoryBottomBar from '../StoryBottomBar/StoryBottomBar';
+import StoryTimeMenu from '../StoryTimeMenu/StoryTimeMenu';
 
 const StoryLayout = () => {
     return (
         <>
             <StoryBar />
+            <StoryTimeMenu />
             <Outlet />
             <StoryBottomBar />
         </>
---
+++ src/components/StoryTimeMenu/StoryTimeMenu.tsx
@@ -0,0 +1,8 @@
+// import { Link } from 'react-router-dom';
+import './StoryTimeMenu.scss';
+
+const StoryTimeMenu = () => {
+    return <aside id='story-time-menu'>side time menu</aside>;
+};
+
+export default StoryTimeMenu;
