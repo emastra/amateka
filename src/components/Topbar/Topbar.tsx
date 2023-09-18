87f5c7ea5fb36405b83ded893b675a45977227f8
emastra <emiliano.mastra@gmail.com>
2023-09-18 15:36:22 +0100

    topbar scroll top

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -1,9 +1,31 @@
+import { useRef, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './Topbar.scss';
 
 const Topbar = () => {
+    let lastScrollTop;
+    const ref = useRef<HTMLInputElement>(null);
+
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            console.log(document.documentElement.scrollTop);
+            if (!ref.current) return;
+            // if (document.documentElement.scrollTop < 64) return;
+
+            const scrollTop = window.scrollY || document.documentElement.scrollTop;
+
+            if (scrollTop > lastScrollTop) {
+                ref.current.style.top = '-64px';
+            } else {
+                ref.current.style.top = '0';
+            }
+
+            lastScrollTop = scrollTop;
+        });
+    }, []);
+
     return (
-        <header className='topbar'>
+        <header ref={ref} className='topbar'>
             <Link to='/' className='logo-wrapper'>
                 <div className='logo-title'>Amateka</div>
             </Link>
