5d0ce7fd489f37da049e6a588bc801d27065dcc7
emastra <emiliano.mastra@gmail.com>
2023-11-03 12:15:48 +0100

    removed scroll transition stuff

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -3,29 +3,8 @@ import { Link } from 'react-router-dom';
 import './Topbar.scss';
 
 const Topbar = () => {
-    // let lastScrollTop;
-    const ref = useRef<HTMLInputElement>(null);
-    // useEffect(() => {
-    //     window.addEventListener('scroll', () => {
-    //         console.log(document.documentElement.scrollTop);
-    //         if (!ref.current) return;
-    //         // if (document.documentElement.scrollTop < 64) return;
-
-    //         const scrollTop = window.scrollY || document.documentElement.scrollTop;
-
-    //         if (scrollTop > lastScrollTop) {
-    //             ref.current.style.top = '-64px';
-    //         } else {
-    //             ref.current.style.top = '0';
-    //         }
-
-    //         lastScrollTop = scrollTop;
-    //     });
-    // }, []);
-    // Pensa di nn usare position absolute. la storybar potrebbe salire da solo se fosse position static. Misurare la diff, rimane spazio bianco qunado topbar che è 64px va su.
-
     return (
-        <header ref={ref} id='topbar'>
+        <header id='topbar'>
             <Link to='/' className='logo-wrapper'>
                 <img className='logo-title' src='/src/assets/svg/amateka-logo.svg' alt='logo' />
             </Link>
