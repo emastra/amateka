586df70c98b84dc02434a4c69ea2ead86ce45b90
emastra <emiliano.mastra@gmail.com>
2023-11-06 15:50:38 +0100

    removed transparent variant backbar (js)

--- src/components/BackBar/BackBar.tsx
+++ src/components/BackBar/BackBar.tsx
@@ -1,46 +1,10 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 import './BackBar.scss';
 
 const BackBar = () => {
-    const [isScrolledOver, setIsScrolledOver] = useState(false);
-
-    useEffect(() => {
-        const handleScroll = () => {
-            if (window.scrollY >= 200 && !isScrolledOver) {
-                setIsScrolledOver(true);
-            } else if (window.scrollY < 200 && isScrolledOver) {
-                setIsScrolledOver(false);
-            }
-        };
-
-        window.addEventListener('scroll', handleScroll);
-
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, [isScrolledOver]);
-
-    // const [scrolling, setScrolling] = useState(false);
-    // const [opacity, setOpacity] = useState(1); // Initialize to fully opaque (1)
-
-    // useEffect(() => {
-    //     const handleScroll = () => {
-    //         const scrollY = window.scrollY;
-    //         const newOpacity = Math.min(scrollY / 200, 1); // Adjust 80 as needed
-    //         setScrolling(scrollY >= 200);
-    //         setOpacity(newOpacity);
-    //     };
-
-    //     window.addEventListener('scroll', handleScroll);
-
-    //     return () => {
-    //         window.removeEventListener('scroll', handleScroll);
-    //     };
-    // }, [scrolling]);
-
     return (
         <>
-            <div id='backbar' className={`${isScrolledOver ? 'solid-variant' : 'transparent-variant'}`}>
+            <div id='backbar'>
                 <div className='back-wrapper'>
                     <img className='back-icon' src='/src/assets/svg/back-arrow.svg' alt='go-back' />
                 </div>
