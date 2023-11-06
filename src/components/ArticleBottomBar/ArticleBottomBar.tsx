5f8d4d318541974f4df70bcf927768f989e3c4c4
emastra <emiliano.mastra@gmail.com>
2023-11-06 18:56:19 +0100

    working on ArticleBottomBar

--- src/components/ArticleBottomBar/ArticleBottomBar.tsx
+++ src/components/ArticleBottomBar/ArticleBottomBar.tsx
@@ -2,7 +2,18 @@ import './ArticleBottomBar.scss';
 import BackBar from '../BackBar';
 
 const ArticleBottomBar = () => {
-    return <div id='articlebottombar'>ArticleBottomBar</div>;
+    return (
+        <div id='articlebottombar'>
+            <div className='content-button-wrapper'>
+                <div>contents</div>
+            </div>
+            <div className='controls-wrapper'>
+                <div>col</div>
+                <div>font</div>
+                <div>more</div>
+            </div>
+        </div>
+    );
 };
 
 export default ArticleBottomBar;
