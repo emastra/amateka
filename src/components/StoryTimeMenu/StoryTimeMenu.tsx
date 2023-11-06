1aa121912ddc6817e913502b038894a691a23e19
emastra <emiliano.mastra@gmail.com>
2023-11-06 15:47:52 +0100

    removed storytimemenu comp

--- src/components/StoryTimeMenu/StoryTimeMenu.tsx
+++
@@ -1,8 +0,0 @@
-// import { Link } from 'react-router-dom';
-import './StoryTimeMenu.scss';
-
-const StoryTimeMenu = () => {
-    return <aside id='story-time-menu'>side time menu</aside>;
-};
-
-export default StoryTimeMenu;
