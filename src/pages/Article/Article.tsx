55afa9c85057fcfa1a10cbf97d055e4bcc23a56f
emastra <emiliano.mastra@gmail.com>
2023-11-06 18:08:01 +0100

    articleId

--- src/pages/Article/Article.tsx
+++ src/pages/Article/Article.tsx
@@ -1,6 +1,10 @@
 import './Article.scss';
+import { useParams } from 'react-router-dom';
 
 const Article = () => {
+    const { articleId } = useParams();
+    console.log('useparams: articleId:', articleId);
+
     return (
         <>
             <main id='article'>article body</main>
