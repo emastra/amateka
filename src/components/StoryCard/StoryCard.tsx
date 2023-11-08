5ba658b5607d3f38ee145a4a22081fa2628e4521
emastra <emiliano.mastra@gmail.com>
2023-11-08 11:01:53 +0100

    StoryCard styles

--- src/components/StoryCard/StoryCard.tsx
+++ src/components/StoryCard/StoryCard.tsx
@@ -11,17 +11,19 @@ const StoryCard = ({ imgurl }) => {
 
                 <div className='card-info-wrapper'>
                     <div className='card-title-wrapper'>
-                        <h2>Storia Contemporanea aorem ipsum</h2>
+                        <h2>Storia Contemporanea</h2>
                     </div>
 
                     <div className='intro-text-wrapper'>
                         <p className='intro-text'>
                             Wikipedia remix. Wikipedia remix. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                            Wikipedia remix.
                         </p>
                     </div>
 
                     <div className='tags-wrapper'>
+                        <div className='tag-box'>A tag 1</div>
+                        <div className='tag-box'>Tag tag 2</div>
+                        <div className='tag-box'>Third tag</div>
                         <div className='tag-box'>A tag 1</div>
                         <div className='tag-box'>Tag tag 2</div>
                         <div className='tag-box'>Third tag</div>
