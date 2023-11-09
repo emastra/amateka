0dc05125406d89b32531fb1e6a323fffc8605c24
emastra <emiliano.mastra@gmail.com>
2023-11-09 15:31:29 +0100

    CardLarge; fixes;

--- src/components/TimelineCard/variants/CardLarge/CardLarge.tsx
+++ src/components/TimelineCard/variants/CardLarge/CardLarge.tsx
@@ -1,31 +1,33 @@
 import { Link } from 'react-router-dom';
 import './CardLarge.scss';
 
-const CardLarge = ({ data }) => {
+const CardLarge = ({ data: { date, image, title, tags } }) => {
     return (
         <article id='card-large'>
-            <Link to={`/story/${'1'}`}>
-                <img
-                    className='card-top-image'
-                    src='/src/assets/images/placeholder_image.jpg'
-                    alt='placeholder_image'
-                />
+            <Link to={`/story/${'1'}`} className='anchor-wrapper'>
+                <div className='card-image-wrapper'>
+                    <div className='card-date'>{date}</div>
+                    {!!image ? <img className='card-top-image' src={image} alt='placeholder_image' /> : null}
+                </div>
+
                 <div className='card-info-wrapper'>
                     <div className='card-title-wrapper'>
-                        <h2>Una decade incredibile</h2>
+                        <h2>{title}</h2>
                     </div>
 
                     <div className='intro-text-wrapper'>
                         <p className='intro-text'>
                             Wikipedia remix. Wikipedia remix. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                            Wikipedia remix.
+                            Wikipedia remix. Lorem ipsum dolor sit amet, consectetur adipiscing elit
                         </p>
                     </div>
 
                     <div className='tags-wrapper'>
-                        <div className='tag-box'>A tag 1</div>
-                        <div className='tag-box'>Tag tag 2</div>
-                        <div className='tag-box'>Third tag</div>
+                        {tags.map((tag) => (
+                            <div key={tag} className='tag-box'>
+                                {tag}
+                            </div>
+                        ))}
                     </div>
                 </div>
             </Link>
--- src/pages/StoryDecades/StoryDecades.tsx
+++ src/pages/StoryDecades/StoryDecades.tsx
@@ -7,16 +7,25 @@ const StoryDecades = () => {
     const { id } = useParams();
     console.log('useparams: id:', id);
 
+    const date = '1970s';
+    const image = '/src/assets/images/placeholder_image.jpg';
+    const image2 = '/src/assets/images/ann-ann.jpg';
+    const image3 = '/src/assets/images/martin-luther-king.jpg';
+    const title = 'Un anno tenebroso.';
+    const description =
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ut turpis eget nunc volutpat vulputate quis non nulla. Cras non euismod.';
+    const tags = ['Santiago'];
+
     return (
         <main id='story-events'>
-            <TimelineCard variant='large' />
-            <TimelineCard variant='large' />
-            <TimelineCard variant='large' />
-            <TimelineCard variant='large' />
-            <TimelineCard variant='large' />
-            <TimelineCard variant='large' />
-            <TimelineCard variant='large' />
-            <TimelineCard variant='large' />
+            <TimelineCard variant='large' data={{ date, image, title, description, tags }} />
+            <TimelineCard variant='large' data={{ date, image: null, title, description, tags }} />
+            <TimelineCard variant='large' data={{ date, image: image2, title, description, tags }} />
+            <TimelineCard variant='large' data={{ date, image: image3, title, description, tags }} />
+            <TimelineCard variant='large' data={{ date, image, title, description, tags }} />
+            <TimelineCard variant='large' data={{ date, image, title, description, tags }} />
+            <TimelineCard variant='large' data={{ date, image, title, description, tags }} />
+            <TimelineCard variant='large' data={{ date, image, title, description, tags }} />
         </main>
     );
 };
