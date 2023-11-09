8f39d55b110098d8ef48ff3acb7e63997bf97ea3
emastra <emiliano.mastra@gmail.com>
2023-11-09 11:59:39 +0100

    better card-date; cardmedium styles;

--- src/components/TimelineCard/variants/CardMedium/CardMedium.tsx
+++ src/components/TimelineCard/variants/CardMedium/CardMedium.tsx
@@ -5,12 +5,8 @@ const CardMedium = ({ data: { date, image, title, description, tags } }) => {
     return (
         <article id='card-medium'>
             <Link to={`/story/${'1'}`} className='anchor-wrapper'>
-                {/* <div className='card-date-wrapper'>
-                    <div className='card-date'>{date}</div>
-                </div> */}
                 <div className='card-image-wrapper'>
                     <div className='card-date'>{date}</div>
-                    {/* <img className='card-top-image' src={image} alt='placeholder_image' /> */}
                     {!!image ? <img className='card-top-image' src={image} alt='placeholder_image' /> : null}
                 </div>
 
@@ -23,11 +19,13 @@ const CardMedium = ({ data: { date, image, title, description, tags } }) => {
                         <p className='intro-text'>{description}</p>
                     </div>
 
-                    {/* <div className='tags-wrapper'>
-                        <div className='tag-box'>A tag 1</div>
-                        <div className='tag-box'>Tag tag 2</div>
-                        <div className='tag-box'>Third tag</div>
-                    </div> */}
+                    <div className='tags-wrapper'>
+                        {tags.map((tag) => (
+                            <div key={tag} className='tag-box'>
+                                {tag}
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </Link>
         </article>
