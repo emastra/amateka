fc829364a7f3a9c9638d3de9815118068c8f211a
emastra <emiliano.mastra@gmail.com>
2023-11-10 11:15:19 +0100

    init List comp

---
+++ src/pages/Article/components/List/List.tsx
@@ -0,0 +1,13 @@
+import './List.scss';
+
+const List = ({ data }) => {
+    console.log('!!', data);
+
+    return (
+        <>
+            <div className='list-container'>list test</div>
+        </>
+    );
+};
+
+export default List;
