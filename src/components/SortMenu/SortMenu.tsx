a2888c8723358f44375a49a18fc2686a80e36aca
emastra <emiliano.mastra@gmail.com>
2023-11-18 16:59:57 +0100

    controlled radio button group

--- src/components/SortMenu/SortMenu.tsx
+++ src/components/SortMenu/SortMenu.tsx
@@ -1,8 +1,14 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
 import './SortMenu.scss';
 
+const options = ['Default', 'Option name 1', 'Option name 2', 'Option name 3', 'Option name 4', 'Option name 5'];
+
 const SortMenu = () => {
-    const options = [];
+    const [selectedOpt, setselectedOpt] = useState(options[0]);
+
+    const handleRadioChange = (event) => {
+        setselectedOpt(event.target.value);
+    };
 
     return (
         <div id='sortmenu'>
@@ -11,11 +17,20 @@ const SortMenu = () => {
             </div>
             <div className='secondary-menu-title'>Ordina per</div>
             <div className='sort-options-wrapper'>
-                <div className='sort-option'>
-                    <label>Option name 1</label>
-                    {/* checked={topping === 'Regular'} */}
-                    <input type='radio' name='sortingBy' value='unnomeee' />
-                </div>
+                {options.map((option) => (
+                    <div className='sort-option'>
+                        <label>
+                            {option}
+                            <input
+                                type='radio'
+                                name='sortingBy'
+                                value={option}
+                                checked={selectedOpt === option}
+                                onChange={handleRadioChange}
+                            />
+                        </label>
+                    </div>
+                ))}
             </div>
         </div>
     );
