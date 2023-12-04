68361da8fe589457f9f113c08aae97de92e6c3cd
emastra <emiliano.mastra@gmail.com>
2023-12-04 16:41:34 +0100

    ColorMenu minor

--- src/components/ColorMenu/ColorMenu.tsx
+++ src/components/ColorMenu/ColorMenu.tsx
@@ -4,12 +4,13 @@ import useArticleMenusContext from '../../hooks/useArticleMenusContext';
 
 const ColorMenu = () => {
     const { toggleColorMenu } = useArticleMenusContext();
+
     return (
         <div id='colormenu'>
             <div className='closing-handle-wrapper'>
                 <div className='closing-handle' onClick={toggleColorMenu}></div>
             </div>
-            <div className='secondary-menu-title'>Ordina per</div>
+            <div className='secondary-menu-title'>Change Color</div>
             <div className=''>test</div>
         </div>
     );
