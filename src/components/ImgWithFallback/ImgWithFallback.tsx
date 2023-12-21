428fdc79be58f3821d61a90e8013748a293f3968
emastra <emiliano.mastra@gmail.com>
2023-12-21 13:02:01 +0100

    do ImgResponsiveWithFallback

---
+++ src/components/ImgWithFallback/ImgWithFallback.tsx
@@ -0,0 +1,5 @@
+// dovrei fare una imgResponsiveWithFallback
+// dovrei portami qui pure gli stili che includono il padding-bottom-trick. e ci deve esse pure il card-image-wrpapper.
+
+// Non l'ho fatto xchè il wrapper include pure il card-date.
+// potrei farlo unsando children e wrappando card-date ?
