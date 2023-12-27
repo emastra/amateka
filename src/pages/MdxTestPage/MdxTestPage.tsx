4d19b59075b6c0cd77bd5e891211d3fcbc49bfd4
emastra <emiliano.mastra@gmail.com>
2023-12-27 17:27:56 +0100

    mdx test page works with dynamic import

--- src/pages/MdxTestPage/MdxTestPage.tsx
+++ src/pages/MdxTestPage/MdxTestPage.tsx
@@ -1,12 +1,31 @@
 import MdxArticle from '../../components/MdxArticle/MdxArticle.mdx';
+// import MdxArticle from 'http://localhost:5173/src/components/MdxArticle/MdxArticle.mdx';
+import React, { useState, useEffect } from 'react';
+// import { MDXProvider } from '@mdx-js/react';
 
 const MdxTestPage = () => {
-    return (
-        <>
-            <div>a test div before the mdx article component</div>
-            <MdxArticle />
-        </>
-    );
+    console.log('!!MdxArticle', MdxArticle);
+    // const [MdxArticle, setMDXContent] = useState<any>(null);
+    const [mdxContent, setMDXContent] = useState<any>(null);
+
+    useEffect(() => {
+        import('../../components/MdxArticle/MdxArticle.mdx')
+            .then((module) => {
+                setMDXContent(module);
+            })
+            .catch((error) => {
+                console.error('Error loading MDX file:', error);
+            });
+    }, []);
+
+    // return (
+    //     <>
+    //         <div>a test div before the mdx article component</div>
+    //         {MdxArticle && <MdxArticle components={{}} />}
+    //     </>
+    // );
+
+    return <div>{mdxContent ? <mdxContent.default components={{}} /> : <p>Loading MDX content...</p>}</div>;
 };
 
 export default MdxTestPage;
